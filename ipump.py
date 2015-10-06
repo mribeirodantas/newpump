ed7e83795beb4de55271d05587625ed7d2104d9c
Marcel Ribeiro Dantas <mribeirodantas@lais.huol.ufrn.br>
2015-10-06 15:29:47 -0300

    Modificado com nomes mais próximos da realidade.

--- ipump.py
+++ ipump.py
@@ -12,15 +12,17 @@ lock = threading.Lock()
 class MyThread(threading.Thread):
     def __init__(self, op):
         if op == 0:
-            threading.Thread.__init__(self, target=self.print1)
+            threading.Thread.__init__(self, target=self.ler_altura)
         elif op == 1:
-            threading.Thread.__init__(self, target=self.print2)
+            threading.Thread.__init__(self, target=self.escrever_tensao)
+        elif op == 2:
+            threading.Thread.__init__(self, target=self.calc_valores)
         else:
-            threading.Thread.__init__(self, target=self.print3)
+            threading.Thread.__init__(self, target=self.conn_supervisorio)
         # Uma flag para notificar a thread de que ela deve parar e dar exit.
         self.kill_received = False
 
-    def print1(self):
+    def ler_altura(self):
         while not self.kill_received:
             lock.acquire()
             tanque = 0
@@ -29,7 +31,7 @@ class MyThread(threading.Thread):
             lock.release()
             sleep(1)
 
-    def print2(self):
+    def escrever_tensao(self):
         global tensao
         while not self.kill_received:
             lock.acquire()
@@ -39,7 +41,14 @@ class MyThread(threading.Thread):
             lock.release()
             sleep(1)
 
-    def print3(self):
+    def calc_valores(self):
+        while not self.kill_received:
+            lock.acquire()
+            print 'Calculando...'
+            lock.release()
+            sleep(1)
+
+    def conn_supervisorio(self):
         global i, tensao
         while not self.kill_received:
             lock.acquire()
@@ -60,6 +69,8 @@ def main():
     threads.append(t)
     t = MyThread(2)
     threads.append(t)
+    t = MyThread(3)
+    threads.append(t)
     for thread in threads:
         thread.daemon = True
     for thread in threads:
@@ -68,7 +79,7 @@ def main():
         # O join basicamente une as threasd para que elas não bloqueiem
         # umas as outras. Filtra-se as threasd que já foram unidas ou
         # retornam None
-        threads = [t.join(2) for t in threads if t is not None and t.isAlive()]
+        threads = [t.join(3) for t in threads if t is not None and t.isAlive()]
 
 if __name__ == '__main__':
     threads = []
