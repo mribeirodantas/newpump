2667bf476dbe38e51933c6d4fb51210e50d71089
Pablo Holanda <phol4nda@gmail.com>
2015-10-08 17:38:21 -0300

    Configuração cascata funcionando.

--- pid.py
+++ pid.py
@@ -22,7 +22,7 @@ def setar_pid(data):
     settings.controle['tipo'] = data['controlador']['tipo']
     settings.controle['tanque'] = data['tanque']
 
-    if settings.controle['tanque'] == 0 and settings.controle['tipo'] == 'simples':
+    if settings.controle['tipo'] == 'simples':
 
         if data['controlador']['controle_1']['tipo'] == 'P':
 
@@ -55,7 +55,40 @@ def setar_pid(data):
             settings.controle['controle_1']['Kd'] = data['controlador']['controle_1']['Kd']
             settings.controle['controle_1']['tipo'] = 5
 
-    elif settings.controle['tanque'] == 1 and settings.controle['tipo'] == 'simples':
+    elif settings.controle['tipo'] == 'cascata':
+
+        if data['controlador']['controle_1']['tipo'] == 'P':
+
+            settings.controle['controle_1']['Kp'] = data['controlador']['controle_1']['Kp']
+            settings.controle['controle_1']['tipo'] = 1
+
+        elif data['controlador']['controle_1']['tipo'] == 'PI':
+
+            settings.controle['controle_1']['Kp'] = data['controlador']['controle_1']['Kp']
+            settings.controle['controle_1']['Ki'] = data['controlador']['controle_1']['Ki']
+            settings.controle['controle_1']['tipo'] = 2
+
+        elif data['controlador']['controle_1']['tipo'] == 'PD':
+
+            settings.controle['controle_1']['Kp'] = data['controlador']['controle_1']['Kp']
+            settings.controle['controle_1']['Kd'] = data['controlador']['controle_1']['Kd']
+            settings.controle['controle_1']['tipo'] = 3
+
+        elif data['controlador']['controle_1']['tipo'] == 'PID':
+
+            settings.controle['controle_1']['Kp'] = data['controlador']['controle_1']['Kp']
+            settings.controle['controle_1']['Ki'] = data['controlador']['controle_1']['Ki']
+            settings.controle['controle_1']['Kd'] = data['controlador']['controle_1']['Kd']
+            settings.controle['controle_1']['tipo'] = 4
+
+        elif data['controlador']['controle_1']['tipo'] == 'PI-D':
+
+            settings.controle['controle_1']['Kp'] = data['controlador']['controle_1']['Kp']
+            settings.controle['controle_1']['Ki'] = data['controlador']['controle_1']['Ki']
+            settings.controle['controle_1']['Kd'] = data['controlador']['controle_1']['Kd']
+            settings.controle['controle_1']['tipo'] = 5
+
+        # Configurar segundo controlador
 
         if data['controlador']['controle_2']['tipo'] == 'P':
 
