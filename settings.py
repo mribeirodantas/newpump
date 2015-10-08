1d24b89fa23a134e5e8df6480e83ff2c13fa0314
Pablo Holanda <phol4nda@gmail.com>
2015-10-08 09:55:08 -0300

    Criado arquivo settings com todas as variáveis globais.

---
+++ settings.py
@@ -0,0 +1,33 @@
+__author__ = 'pabloholanda'
+
+
+def init():
+    global tanque, sinal, controle
+    tanque = {
+        'pvtq_1': 0.00,
+        'pvtq_2': 0.00,
+        'mvtq_1': 0.00,
+        'mvtq_2': 0.00,
+        'sptq_1': 0.00,
+        'sptq_2': 0.00,
+        'tanque': 0
+    }
+
+    sinal = {
+        'tipo_sinal': 0,
+        'deslocamento': 0,
+        'periodo': 0,
+        'amp_min': 0,
+        'amp_max': 0,
+        'offset': 0,
+        'duracao': 0
+    }
+
+    controle = {
+        'Kp': 0.000,
+        'Ki': 0.000,
+        'Kd': 0.000,
+        'P': 0.000,
+        'I': 0.000,
+        'D': 0.000,
+    }
