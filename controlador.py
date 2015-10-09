cd5f4520e158b8cc0de343c8a45797bcd29d0301
Pablo Holanda <phol4nda@gmail.com>
2015-10-08 21:21:35 -0300

    Corrigindo bug que não alterava a variavel correta de tensão.

--- controlador.py
+++ controlador.py
@@ -6,6 +6,15 @@ import sinal
 
 def setar_tensao(t):
     if settings.controle['malha_aberta']:
-        return sinal.gerar_sinal(t)
+        if settings.controle['tanque'] == 0:
+
+            settings.tanque['mvtq_1'] = sinal.gerar_sinal(t)
+            return settings.tanque['mvtq_1']
+
+        elif settings.controle['tanque'] == 1:
+
+            settings.tanque['mvtq_2'] = sinal.gerar_sinal(t)
+            return settings.tanque['mvtq_2']
+
     else:
         return 0
--- tratar_info.py
+++ tratar_info.py
@@ -11,13 +11,20 @@ def enviar_parametros():
 def setar_tipo_controle(data):
     try:
         if data['malha_aberta']:
-            settings.controle['controle'] = True
+            settings.controle['malha_aberta'] = True
+            if data['tanque'] == 0:
+                settings.controle['tanque'] = 0
+                settings.tanque['mvtq_2'] = 0.00
+            else:
+                settings.controle['tanque'] = 1
+                settings.tanque['mvtq_1'] = 0.00
+
             sinal.setar_sinal(data)
             print settings.sinal
         else:
             sinal.setar_sinal(data)
             pid.setar_pid(data)
-            settings.controle['controle'] = False
+            settings.controle['malha_aberta'] = False
             print settings.sinal
             print settings.controle
     except:
